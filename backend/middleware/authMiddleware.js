3f5fe4126df876bf7720066e43c39cd936afd13f
Fucci <benjaminakacha@gmail.com>
2024-12-30 19:24:01 -0800

    jwt secret

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,27 +1,15 @@
 // backend/middleware/authMiddleware.js
 const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
-// const authMiddleware = (req, res, next) => {
-//   const token = req.header('Authorization')?.split(' ')[1];
-//   if (!token) return res.status(401).json({ message: 'No token, authorization denied' });
-
-//   try {
-//     const decoded = jwt.verify(token, 'your_jwt_secret');
-//     req.user = decoded.userId;
-//     next();
-//   } catch (error) {
-//     res.status(401).json({ message: 'Token is not valid' });
-//   }
-// };
-
-// module.exports = authMiddleware;
+const jwtSecret = process.env.JWT_SECRET;
 
 const authMiddleware = (req, res, next) => {
   const token = req.header('Authorization')?.split(' ')[1]; // Extract token from 'Authorization' header
   if (!token) return res.status(401).json({ message: 'No token, authorization denied' });
   try {
     // Decode the token using your JWT secret
-    const decoded = jwt.verify(token, 'your_jwt_secret'); // Make sure to replace 'your_jwt_secret' with your actual secret
+    const decoded = jwt.verify(token, jwtSecret); // Make sure to replace 'your_jwt_secret' with your actual secret
 
     // Attach the entire decoded payload (including userId) to req.user
     req.user = decoded; // You can store the entire decoded payload for more flexibility
